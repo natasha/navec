29f83126c90cffde23641491d33089867718c979
Kukushkin Alexander <alex@alexkuk.ru>
2019-05-30 15:54:19 +0300

    Better report

--- navec/eval/report.py
+++ navec/eval/report.py
@@ -4,16 +4,16 @@ import pandas as pd
 
 def format_mb(bytes):
     mb = bytes / 1024 / 1024
-    return '%0.2f' % mb
+    return '%0.1f' % mb
 
 
 def format_sec(secs):
-    return '%0.2f' % secs
+    return '%0.1f' % secs
 
 
 def format_mks(secs):
     mks = secs * 1000000
-    return '%0.2f' % mks
+    return '%0.1f' % mks
 
 
 def report_table(records, schemes, datasets):
@@ -24,18 +24,17 @@ def report_table(records, schemes, datasets):
             record.name,
             format_sec(record.stats.init.value),
             format_mks(record.stats.get.value),
-            format_mks(record.stats.sim.value),
             format_mb(record.stats.disk),
             format_mb(record.stats.ram)
         ]
         for name in names:
             score = record.scores[name]
-            row.append('%0.3f / %d' % (score.value, score.support))
+            row.append('%0.3f | %d' % (score.value, score.support))
         data.append(row)
 
     table = pd.DataFrame(
         data,
-        columns=['model', 'init, s', 'get, µs', 'sim, µs', 'disk, mb', 'ram, mb'] + names
+        columns=['model', 'init, s', 'get, µs', 'disk, mb', 'ram, mb'] + names
     )
     table = table.set_index('model')
     names = [_.name for _ in schemes]
