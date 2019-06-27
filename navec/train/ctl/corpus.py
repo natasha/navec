dcb05924e8bbf2edca1c264fe960480f2f7ffd5f
Kukushkin Alexander <alex@alexkuk.ru>
2019-06-27 14:21:04 +0300

    Readers for datasets other then librusec

--- navec/train/ctl/corpus.py
+++ navec/train/ctl/corpus.py
@@ -1,6 +1,31 @@
 
 LIBRUSEC = 'librusec'
-CORPORA = [LIBRUSEC]
+TAIGA_PROZA = 'taiga_proza'
+
+WIKI = 'wiki'
+
+LENTA = 'lenta'
+RIA = 'ria'
+TAIGA_FONTANKA = 'taiga_fontanka'
+BURIY_NEWS = 'buriy_news'
+BURIY_WEBHOSE = 'buriy_webhose'
+ODS_GAZETA = 'ods_gazeta'
+ODS_INTERFAX = 'ods_interfax'
+
+CORPORA = [
+    LIBRUSEC,
+    TAIGA_PROZA,
+
+    WIKI,
+
+    LENTA,
+    RIA,
+    TAIGA_FONTANKA,
+    BURIY_NEWS,
+    BURIY_WEBHOSE,
+    ODS_GAZETA,
+    ODS_INTERFAX,
+]
 
 
 def corpus_read(args):
@@ -13,7 +38,18 @@ def corpus_read_(name, path):
     import corus
 
     functions = {
-        LIBRUSEC: corus.load_librusec
+        LIBRUSEC: corus.load_librusec,
+        TAIGA_PROZA: corus.load_taiga_proza,
+
+        WIKI: corus.load_wiki,
+
+        LENTA: corus.load_lenta,
+        RIA: corus.load_ria,
+        TAIGA_FONTANKA: corus.load_taiga_fontanka,
+        BURIY_NEWS: corus.load_buriy_news,
+        BURIY_WEBHOSE: corus.load_buriy_webhose,
+        ODS_GAZETA: corus.load_ods_gazeta,
+        ODS_INTERFAX: corus.load_ods_interfax
     }
     load = functions[name]
     records = load(path)
