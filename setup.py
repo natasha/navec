e558ce2ce3ba5de334bdf53fc96e3f2ce78fd443
Kukushkin Alexander <alex@alexkuk.ru>
2019-08-28 14:37:59 +0300

    Up version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='navec',
-    version='0.6.0',
+    version='0.7.0',
     install_requires=['numpy'],
     packages=find_packages(),
     entry_points={
