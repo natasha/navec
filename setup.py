afea738d5cd84a03766980d250dcdb5c1fe82c76
Kukushkin Alexander <alex@alexkuk.ru>
2020-03-20 14:59:54 +0300

    Up setup

--- setup.py
+++ setup.py
@@ -1,14 +1,37 @@
 
 from setuptools import setup, find_packages
 
+
+with open('README.md') as file:
+    description = file.read()
+
+
+with open('requirements/main.txt') as file:
+    requirements = [_.strip() for _ in file]
+
+
 setup(
     name='navec',
     version='0.7.0',
-    install_requires=['numpy'],
+    description='Compact high quality word embeddings for russian language',
+    long_description=description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/natasha/navec',
+    author='Alexander Kukushkin',
+    author_email='alex@alexkuk.ru',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    keywords='embeddings, word2vec, glove, nlp, russian, quantization',
     packages=find_packages(),
+    install_requires=requirements,
     entry_points={
         'console_scripts': [
             'navec-train=navec.train.ctl.__main__:main'
         ]
     }
 )
+
